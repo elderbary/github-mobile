35c05be16c6f2b1bb740447f5b075f015f79d122
Dawid Różański <drozanski@outlook.com>
2022-01-09 15:05:14 +0100

    add first screen

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+
+class TextInANest extends Component {
+  state = {};
+
+  render() {
+    return <Text>Home</Text>;
+  }
+}
+
+export default TextInANest;
